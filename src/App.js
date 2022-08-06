24fd97ccbd8ba4dc669404fe0039c5b0b57f9a67
JANWE <jan.wegmann@hotmail.de>
2022-08-06 23:16:27 +0200

    change api data for env file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import { useState } from 'react'
-import TextBox from './components/TextBox'
-import Arrows from './components/Arrows'
-import Modal from './components/Modal'
-import Button from './components/Button'
-
+import { useEffect, useState } from "react";
+import TextBox from "./components/TextBox";
+import Arrows from "./components/Arrows";
+import Modal from "./components/Modal";
+import Button from "./components/Button";
+import axios from "axios";
 
 function App() {
+  const [showModal, setShowModal] = useState(null);
+  const [inputLanguage, setInputLanguage] = useState("German");
+  const [outputLanguage, setOutputLanguage] = useState("English");
+  const [languages, setLanguages] = useState(null);
 
-  const [inputLanguage, setInputLanguage] = useState('German');
-  const [outputLanguage, setOutputLanguage] = useState('English');
+  const getLanguages = () => {
+    const axios = require("axios");
+
+    const options = {
+      method: "GET",
+      url: "http://localhost:8000/languages",
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        Object.keys(response.data.data)
+        setLanguages(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  console.log("languages", languages);
+
+  useEffect(() => {
+      getLanguages()
+  }, [])
 
   const handleClick = () => {
-    setInputLanguage(outputLanguage)
-    setOutputLanguage(inputLanguage)
-  }
+    setInputLanguage(outputLanguage);
+    setOutputLanguage(inputLanguage);
+  };
+
+  console.log("showModal", showModal);
 
   return (
     <div className="app">
-      <TextBox selectedLanguage={inputLanguage} style={'input'} />
-      <div className='arrow-container' onClick={handleClick}>
-      <Arrows />
-      </div>
-      <TextBox selectedLanguage={outputLanguage} style={'output'} />
+      {!showModal && (
+        <>
+          {" "}
+          <TextBox
+            selectedLanguage={inputLanguage}
+            style={"input"}
+            setShowModal={setShowModal}
+          />
+          <div className="arrow-container" onClick={handleClick}>
+            <Arrows />
+          </div>
+          <TextBox
+            selectedLanguage={outputLanguage}
+            style={"output"}
+            setShowModal={setShowModal}
+          />
+        </>
+      )}
+      {showModal && <Modal setShowModal={setShowModal} />}
     </div>
   );
 }
